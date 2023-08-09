be581557d0bf1c91c70dfafeffa724ba4c3bae28
Johan Guevara <johanguevara1221@gmail.com>
2023-08-09 14:07:30 -0300

    New Hosts

--- src/server/server.ts
+++ src/server/server.ts
@@ -27,7 +27,7 @@ export default class Server {
     this.app.use(bodyParser.urlencoded({ extended: true }));
     this.app.use(bodyParser.json());
     this.app.use(cors({
-      origin: ['https://guidden.netlify.app', 'https://taskapp-53xk.onrender.com'],
+      origin: ['https://guidden.netlify.app', 'https://stakyapp.web.app'],
       optionsSuccessStatus: 200,
       credentials: true
     }));
